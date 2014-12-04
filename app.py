6c46f3b01c9e25bcecd2b62057f68f936e2f7bdc
tvhung83 <ng.hung83@gmail.com>
2014-12-04 14:49:20 +0700

    Add action to get by id

--- app.py
+++ app.py
@@ -4,29 +4,42 @@
 import os
 import bottle
 import pymongo
+import json
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 from bottle import response, route, get, run
-from json import dumps
 from sys import argv
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 @get('/<code>')
 @get('/<code>/<page:int>')
 def index(code = 'phim-le', page = 1):
-    MONGO_URL = os.environ.get('MONGOHQ_URL', \
-        'mongodb://heroku:nw9jO-6c1dWBHT9OH3z6k3ZDBuFmO9dpuY4BfI0L8ZEjs8DEnm8K0taSizRAt5G1s8Kb5FjxwxsqiOSStIII1A@dogen.mongohq.com:10092/app32117576')
-    client = MongoClient(MONGO_URL)
-    # Specify the database
-    db = client.get_default_database()
     results = db.items.find({'categories': code }, \
-                { 'title': 1, 'thumbnail': 1, 'links': 1, '_id': 0 } ) \
+                { 'title': 1, 'thumbnail': 1, 'links': 1 } ) \
                 .sort('publishedAt',  pymongo.DESCENDING) \
                 .skip((page-1)*20 if page > 1 else 0) \
                 .limit(20)
     response.content_type = 'application/json'
-    return dumps(list(results))
+    return JSONEncoder().encode(list(results))
+
+@get('/id/<id>')
+def index(id):
+    item = db.items.find_one({'_id': ObjectId(id) }, { 'thumbnail': 1, 'links': 1, '_id': 0 } )
+    response.content_type = 'application/json'
+    return JSONEncoder().encode(item)
 
 @get('/health')
 def health():
     return "ok"
-    
+
+MONGO_URL = os.environ.get('MONGOHQ_URL', \
+    'mongodb://heroku:nw9jO-6c1dWBHT9OH3z6k3ZDBuFmO9dpuY4BfI0L8ZEjs8DEnm8K0taSizRAt5G1s8Kb5FjxwxsqiOSStIII1A@dogen.mongohq.com:10092/app32117576')
+client = MongoClient(MONGO_URL)
+# Specify the database
+db = client.get_default_database()
 bottle.run(host='0.0.0.0', port=argv[1], debug=False, reloader=False)
